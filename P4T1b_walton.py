35e102ac37276767a1f8e9da48832a97b34becb5
walton0193 <61675664+walton0193@users.noreply.github.com>
2020-03-30 23:32:36 -0400

    Add files via upload

---
+++ P4T1b_walton.py
@@ -0,0 +1,20 @@
+#Kenneth Walton
+#CTI 110
+#March 30 2020
+#
+
+import turtle
+turtle.penup()
+turtle.goto(0,100)
+turtle.pendown()
+turtle.goto(0,0)
+turtle.goto(0,50)
+turtle.penup()
+turtle.goto(100,100)
+turtle.pendown()
+turtle.goto (0,50)
+turtle.goto(100,0)
+
+
+
+
